ea61b23b0feac90b06005f9f10fd12d0a6cee643
MuhammadEmadHussein <mohamedemadhu01019@gmail.com>
2023-03-13 06:17:08 +0200

    'ch5'

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local
     'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser" 
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth import get_user_model
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "home.html"
